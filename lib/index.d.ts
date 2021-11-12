eea10cfa13f482f60a819391385437b6ea80ec92
benho-ijs <benho@instajs.com>
2021-11-12 15:31:51 +0800

    rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,1 @@
-export { VRFRequestIDBase } from './@chainlink/contracts/src/v0.8/VRFRequestIDBase';
-export { SafeERC20 } from './@openzeppelin/contracts/token/ERC20/utils/SafeERC20';
-export { ERC721 } from './@openzeppelin/contracts/token/ERC721/ERC721';
-export { Address } from './@openzeppelin/contracts/utils/Address';
-export { Strings } from './@openzeppelin/contracts/utils/Strings';
-export { Authorization } from './Authorization';
-export { TrollNFT } from './TrollNFT';
-export { TrollNFT_VRF } from './TrollNFT_VRF';
+export * as Contracts from './contracts';
